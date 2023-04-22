634553b111032ddabd27eb2a0013f11bf27f3321
Abner Qian <abner@playhouse.so>
2023-04-21 20:22:45 -0700

    Move navigation types into a single file.

--- src/layouts/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-
-export type RootStackParamList = {
-  Home: undefined;
-  RecordVideo: undefined;
-};
-
-export type RootStackNavigationProp =
-  NativeStackNavigationProp<RootStackParamList>;
