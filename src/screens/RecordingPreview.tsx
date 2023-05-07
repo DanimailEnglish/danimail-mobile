6913c2461b230ab8e8920a6c30e7483852d087ee
Abner Qian <abner@playhouse.so>
2023-05-06 17:09:50 -0700

    Add confirmation alerts to the recording preview screen. Wrap navigation.setOptions in a useEffect.

--- src/screens/RecordingPreview.tsx
+++ src/screens/RecordingPreview.tsx
@@ -1,7 +1,7 @@
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Button} from '@rneui/themed';
-import React, {useCallback} from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useCallback, useEffect, useRef} from 'react';
+import {Alert, StyleSheet} from 'react-native';
 import Video from 'react-native-video';
 
 import type {RootStackParamList} from '../layouts';
@@ -17,18 +17,57 @@ export function RecordingPreviewScreen({
   },
   navigation,
 }: RecordingPreviewScreenProps): JSX.Element {
+  const sentVideo = useRef(false);
+
   const handleSend = useCallback(() => {
-    console.log('send the video');
-  }, []);
+    Alert.alert('Send video?', 'Are you sure you want to send this video?', [
+      {text: "Don't send", style: 'cancel', onPress: () => {}},
+      {
+        text: 'Send',
+        style: 'default',
+        onPress: () => {
+          console.log('Start the upload here');
+          sentVideo.current = true;
+          navigation.navigate('Home');
+        },
+      },
+    ]);
+  }, [navigation]);
 
   const memoizedSendButton = useCallback(
     () => <Button type="clear" title="Send" onPress={handleSend} />,
     [handleSend],
   );
 
-  navigation.setOptions({
-    headerRight: memoizedSendButton,
-  });
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: memoizedSendButton,
+    });
+  }, [memoizedSendButton, navigation]);
+
+  useEffect(() => {
+    const subscriber = navigation.addListener('beforeRemove', e => {
+      if (sentVideo.current) {
+        return;
+      }
+
+      e.preventDefault();
+      Alert.alert(
+        'Discard video?',
+        'You have not sent this video yet. Are you sure you want to discard this recording?',
+        [
+          {text: "Don't leave", style: 'cancel'},
+          {
+            text: 'Discard',
+            style: 'destructive',
+            onPress: () => navigation.dispatch(e.data.action),
+          },
+        ],
+      );
+    });
+
+    return subscriber;
+  }, [navigation]);
 
   return (
     <Video
